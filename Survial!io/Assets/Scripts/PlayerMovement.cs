3129535ee7e0613440c5ff368bc204f52776193f
Olive You <cheeeedy@gmail.com>
2023-04-03 20:33:08 +0900

    [Feat] Sprite Flipping by moving direction
    Sprites are flipped by players input.

--- Survial!io/Assets/Scripts/PlayerMovement.cs
+++ Survial!io/Assets/Scripts/PlayerMovement.cs
@@ -8,14 +8,24 @@ public class PlayerMovement : MonoBehaviour
     public float _speed = 4f;
     private Vector2 _inputVec;
     private Rigidbody2D _rigidbody;
+    
+    private SpriteRenderer _renderer;
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<SpriteRenderer>();
     }
     private void FixedUpdate()
     {
         Vector2 nextVec = _inputVec.normalized * _speed * Time.fixedDeltaTime;
         _rigidbody.MovePosition(_rigidbody.position + nextVec);
+    }
+    private void LateUpdate()
+    {        
+        if (_inputVec.x != 0)
+        {
+            _renderer.flipX = _inputVec.x < 0;
+        }
     }
     private void OnMove(InputValue value)
     {

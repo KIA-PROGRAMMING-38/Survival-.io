cc3bcc3afbab484c42c0efd766dc0b4b79240be2
Olive You <cheeeedy@gmail.com>
2023-04-10 21:40:58 +0900

    [Feat] Prototype of Skill System
    - Each skill will shoot bullets.
    - In this commit, all datas are temporary.
    - Need additional function of linking datas with datatable.
    - This commit includes changing directories of scripts.
    - This commit includes additional sprites for bullet.

---
+++ Survial!io/Assets/Scripts/Skill/Bullet.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector2 _moveDirection;
+    private float _speed;
+    private float _elapsedTime;
+    private float _activateTime = 1f;
+    private Transform _target;
+    // range를 어떻게 설정해 줄 것인가 - 스킬 세부 구현에서 결정 
+
+    public ObjectPool<Bullet> Pool { private get; set; }
+
+    private Rigidbody2D _rigidbody;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        Initialize();
+        Move();
+    }
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime >= _activateTime)
+        {
+            _elapsedTime = 0f;
+            Deactivate();
+        }
+    }
+    private void Initialize()
+    {
+        _rigidbody.gravityScale = 0;
+
+        RigidbodyConstraints2D constraints = RigidbodyConstraints2D.FreezeRotation;
+        _rigidbody.constraints = constraints;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Deactivate();
+        }
+    }
+    private void Move()
+    {
+        Transform bullet;
+        bullet = GetComponent<Transform>();
+        _moveDirection = transform.forward;
+        _speed = 3f;
+        bullet.Translate(_moveDirection * _speed, Space.World);
+        
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+        Pool.Release(this);
+    }
+}
